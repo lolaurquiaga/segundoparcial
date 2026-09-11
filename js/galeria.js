4a484808ddaa2852058c6c5c7ec8563896deccb1
lolaurquiaga <lolaurquiaga@gmail.com>
2026-09-11 17:03:18 -0300

    Modificación a la galería
    Se ve más prolija la galería. Si puedo la seguiré trabajando.
    Elimino el ejercicio.js que iba a ser interactivo con la galería.

--- js/galeria.js
+++ js/galeria.js
@@ -1,14 +1,13 @@
 // Array de objetos con las obras
 const obras = [
     { nombre: "Pulse Room", anio: 2006, imagen: "img/pulseRoom.jpg" },
+    { nombre: "Vicious Circular Breathing", anio: 2013, imagen: "img/viciousCircularBreathing.jpg" },
+    { nombre: "Solar Equation", anio: 2010, imagen: "img/solarEquation.jpg" },
     { nombre: "Vectorial Elevation", anio: 1999, imagen: "img/vectorialElevation.jpg" },
     { nombre: "Body Movies", anio: 2001, imagen: "img/bodyMovies.jpg" },
-    { nombre: "33 Questions per Minute", anio: 2000, imagen: "img/33questionsPerMinute.jpg" },
     { nombre: "Voz Alta", anio: 2008, imagen: "img/vozAlta.jpg" },
-    { nombre: "Solar Equation", anio: 2010, imagen: "img/solarEquation.jpg" },
     { nombre: "Pan-Anthem", anio: 2015, imagen: "img/panAnthem.jpg" },
     { nombre: "Level of Confidence", anio: 2015, imagen: "img/levelOfConfidence.jpg" },
-    { nombre: "Vicious Circular Breathing", anio: 2013, imagen: "img/viciousCircularBreathing.jpg" },
     { nombre: "Border Tuner", anio: 2019, imagen: "img/borderTuner.jpg" }
 ];
 
